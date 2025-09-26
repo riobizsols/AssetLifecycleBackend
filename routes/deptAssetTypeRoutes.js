8aca4585c54da4977c4bccddbf49989c2f65cd3c
NarenRio <narenrio756@gmail.com>
2025-09-26 10:38:46 +0530

    Bug-Department Asset Types

--- routes/deptAssetTypeRoutes.js
+++ routes/deptAssetTypeRoutes.js
@@ -7,6 +7,7 @@ const { protect } = require("../middlewares/authMiddleware");
 router.use(protect);
 
 router.get("/asset-types", controller.getAllAssetTypes);  // /api/dept-assets/asset-types
+router.get("/department/:dept_id/asset-types", controller.getAssetTypesByDepartment);  // /api/dept-assets/department/:dept_id/asset-types
 router.get("/", controller.fetchAllDeptAssets);           // /api/dept-assets/
 router.post("/", controller.addDeptAsset);                // /api/dept-assets/
 router.delete("/", controller.deleteDeptAsset);           // /api/dept-assets/
