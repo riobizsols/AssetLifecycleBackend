d3a5555de594195273a9ed69e7de08b855c906d3
Sanjana S S <sanjanass23mss048@skasc.ac.in>
2026-09-25 16:27:28 +0530

    Revert "Add Spare Parts Report API with category, brand, status, and purchase date filters."
    This reverts commit a144e820ee729cddc0cafbe42e7d9d00dc6f43ba.

--- routes/sparePartsRoutes.js
+++ routes/sparePartsRoutes.js
@@ -53,10 +53,6 @@ const {
   getEquipmentWise,
   getHoldDuration,
 } = require('../controllers/sparePartManagementController');
-const {
-  getFilterOptions: getSparePartsReportFilterOptions,
-  getReport: getSparePartsReport,
-} = require('../controllers/sparePartsReportController');
 
 router.use(protect);
 
@@ -118,8 +114,5 @@ router.get('/management/slow-non-moving', getSlowNonMoving);
 router.get('/management/consumption', getSparePartConsumption);
 router.get('/management/equipment-wise', getEquipmentWise);
 router.get('/management/hold-duration', getHoldDuration);
-
-router.get('/report/filter-options', getSparePartsReportFilterOptions);
-router.get('/report', getSparePartsReport);
 
 module.exports = router;
